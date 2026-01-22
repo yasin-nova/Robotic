ce5af3637f78c38cc95dea3aa305ff122b155574
yasin-nova <yasin.nova.dev@gmail.com>
2026-01-22 22:33:53 +0300

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import Board from "./components/Board"
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>🤖 Robotik Kodlama Oyunu</h1>
+      <Board />
+    </div>
+  )
+}
