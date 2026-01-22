74f9a7d2c35560c3ccd4c0b5bad9f32cef332d43
yasin-nova <yasin.nova.dev@gmail.com>
2026-01-22 23:14:33 +0300

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Board from "./components/Board"
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "10px" }}>
+        🤖 Robotik Kodlama Oyunu
+      </h1>
+      <Board />
+    </div>
+  )
+}
